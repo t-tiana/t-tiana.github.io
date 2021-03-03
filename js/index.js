17b5d0f17596cf63ffd937208cc8c0484b966fc5
t-tiana <t.tiana@keemail.me>
2021-03-03 17:01:40 +0200

    mobile menu done

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("o-mobileNav").style.height = "100%";
+    document.getElementById("o-mobileNav").style.top = "0";
+}
+function closeNav() {
+    document.getElementById("o-mobileNav").style.height = "0";
+    document.getElementById("o-mobileNav").style.top = "50%";
+}
